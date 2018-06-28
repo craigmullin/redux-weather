ea65550b246732f84278714c765eaebd0edb22da
Craig Mullin <cmullin@dreeshomes.com>
2018-06-28 18:52:03 -0400

    thru lesson 66

---
+++ src/components/chart.js
@@ -0,0 +1,19 @@
+import _ from 'lodash';
+import React from 'react';
+import Trend from 'react-trend';
+// this is a functional component, not a container
+
+function average(data){
+    return _.round(_.sum(data)/data.length);
+}
+
+
+
+export default (props) => {
+    return (
+        <div>
+            <Trend height={120} width={180} stroke={props.color} strokeWidth="2px" data={props.data}></Trend>
+            <div>Average this week = {average(props.data)} {props.units}</div>
+        </div>
+    );
+}
---
+++ src/containers/weather-list.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Chart from '../components/chart';
+
+class WeatherList extends Component {
+  renderWeather(cityData){ // to render a single row
+    const name = cityData.city.name;
+    const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => (((temp - 273) * 9/5) + 32));
+    const pressures = _.map(cityData.list.map(weather => weather.main.pressure), (pressure) => pressure * .03);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+
+
+    return(
+      <tr key={name}>
+        <td>{name}</td>
+        <td><Chart data={temps} color="orange" units="F" /></td>
+        <td><Chart data={humidities} color="blue" units="%" /></td>
+        <td><Chart data={pressures} color="green" units="inHg" /></td>
+      </tr>
+    );
+  }  
+  
+  render() {
+        return (
+            <table className="table table-hover">
+              <thead>
+                <tr>
+                  <th>City</th>
+                  <th>Temerature</th>
+                  <th>Humidity</th>
+                  <th>Pressure</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.props.weather.map(this.renderWeather)}
+              </tbody>
+            </table>
+        )
+    }
+}
+
+// function mapStateToProps(state){
+//   return { weather: state.weather };
+// }
+// Above is the long version of below...
+
+function mapStateToProps({ weather }){
+  return { weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);
